506e68974c1dc5f9088baeb4f5ece4ec2198a327
Lucas Sousa <lsousatop5771@gmail.com>
2026-05-02 21:49:13 -0300

    feat: implement products API route and add serialization to DataTable component

---
+++ app/api/products/route.ts
@@ -0,0 +1,36 @@
+import { db } from "@/app/_lib/db";
+
+export async function GET() {
+  const products = await db.product.findMany({});
+
+  return Response.json(products, {
+    status: 200,
+  });
+}
+
+export async function POST(request: Request) {
+  const body = await request.json();
+
+  const name = body.name;
+
+  const price = body.price;
+
+  const stock = body.stock;
+
+  await db.product.create({
+    data: {
+      name,
+      price,
+      stock,
+    },
+  });
+
+  return Response.json(
+    {
+      message: "Produto criado com sucesso",
+    },
+    {
+      status: 201,
+    },
+  );
+}
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -5,7 +5,14 @@ import { columns } from "./_components/table-columns";
 import { getProducts } from "../_data-access/products/get-products";
 
 const Products = async () => {
+  // Usar na maioria dos casos Data Access Layer
   const products = await getProducts();
+
+  // Usar quando precisar de cache e a aplicacao tiver webhooks externos para expor a aplicacao
+  // const response = await fetch("http://localhost:3000/api/products", {
+  //   cache: "no-store",
+  // });
+  // const products = await response.json();
 
   return (
     <div className="h-full space-y-8 bg-white p-8">
@@ -15,7 +22,10 @@ const Products = async () => {
         buttonText="Novo Produto"
         buttonIcon={<PlusIcon />}
       />
-      <DataTable columns={columns} data={products} />
+      <DataTable
+        columns={columns}
+        data={JSON.parse(JSON.stringify(products))} // Serialização e desserialização para remover propriedades não serializáveis do prisma Ex: Decimal
+      />
     </div>
   );
 };
